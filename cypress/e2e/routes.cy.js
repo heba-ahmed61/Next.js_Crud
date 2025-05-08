c1fd95613e9ebb96d91f0aeb7b1ed0e65812b469
heba.fahmy <heba.fahmy@cyshield.com>
2025-05-08 13:20:54 +0300

    testing for existing item at build time at dynamic route

--- cypress/e2e/routes.cy.js
+++ cypress/e2e/routes.cy.js
@@ -3,3 +3,9 @@ it('displays correct heading at about page', () => {
   cy.findByRole('link', { name: /about page/i }).click();
   cy.findByRole('heading', { name: /about page/i }).should('exist');
 });
+
+// testing for existing item at build time at dynamic route
+it('testing for existing item at build time at dynamic route', () => {
+  cy.visit('/posts/details/2025-02-24T12:00:38.403Z');
+  cy.findByRole('link', { name: /first item from dev/i }).should('exist');
+});
