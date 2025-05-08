a9be47928ecec0444c6bd35b8ba658a27bacf95f
heba.fahmy <heba.fahmy@cyshield.com>
2025-05-08 14:07:43 +0300

    issue at  testing for existing item but after build time but test is written

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,34 @@
+// import { defineConfig } from "cypress";
+// import { addPost } from "./app/actions/serverActions";
+
+// export default defineConfig({
+//   e2e: {
+//     baseUrl:'http://localhost:3000',
+//     setupNodeEvents(on, config) {
+//      on('task',{
+//       'addItem': (item) => {
+//         addPost(item).then(() => null)
+//       }
+//      })
+//     },
+//   },
+// });
+
 import { defineConfig } from "cypress";
+import { addPost } from "./app/actions/serverActions";
 
 export default defineConfig({
   e2e: {
-    baseUrl:'http://localhost:3000',
+    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('task', {
+        // addItem(item) {
+        //   return addPost(item).then(() => null); // Ensure to return the promise
+        // },
+        'addItem': (item) => {
+          return addPost(item).then(()=> null)
+        }
+      });
     },
   },
 });
--- cypress/e2e/routes.cy.js
+++ cypress/e2e/routes.cy.js
@@ -1,3 +1,5 @@
+const { addPost } = require('../../app/actions/serverActions');
+
 it('displays correct heading at about page', () => {
   cy.visit('/');
   cy.findByRole('link', { name: /about page/i }).click();
@@ -10,7 +12,26 @@ it('testing for existing item at build time at dynamic route', () => {
   cy.findByRole('link', { name: /first item from dev/i }).should('exist');
 });
 
+// testing for not existing item at build time at dynamic route
 it('testing for not existing item at build time at dynamic route', () => {
   cy.visit('/posts/details/61');
   cy.findByRole('link', { name: /first item from dev/i }).should('not.exist');
 });
+
+// testing for existing item but after build time so now the version of build i run not have this item at dynamic route
+// it('testing for existing item but after build item so now the version of build I run not have this item at dynamic route', () => {
+//   const data = {
+//     userId: 1,
+//     id: 62,
+//     title: 'add item not at build for test',
+//     body: 'add item not at build for test',
+//   };
+
+//   // Pass the data directly to the task
+//   cy.task('addItem', data).then(() => {
+//     cy.visit('/posts/details/62'); // Visit the page after the item has been added
+//     cy.findAllByRole('heading', {
+//       name: /add item not at build for test/i,
+//     }).should('exist'); // Assert that the heading exists
+//   });
+// });
